bdd248644e92afdc5f03694f1b0541a98207fa41
Guilherme de Castro <guilycst@gmail.com>
2024-05-19 22:58:59 -0300

    feat(main.go): implement health check endpoint to verify database connection status feat(main.go): add support for POST method to shorten URLs and return shortened URL feat(main.go): update GET method to resolve shortened URLs and redirect to original URL feat(main.go): refactor variable name 'host' to 'serverAddress' for clarity feat(main.go): refactor variable name 'url' to 'originalUrl' for clarity feat(main.go): refactor variable name 'short' to 'shortener' for clarity feat(main.go): refactor variable name 'gen' to 'idGenerator' for clarity

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,90 @@
 package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+	"net/url"
+
+	"github.com/guilycst/shortee/internal/services"
+)
+
+type Short struct {
+	Url string `json:"url"`
+}
+
+func main() {
+
+	host := "localhost:8080"
+	db, err := sql.Open("sqlite3", "db/shortee.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	gen, err := services.NewSQLiteBigIntGenerator(db)
+	if err != nil {
+		panic(err)
+	}
+
+	short, err := services.NewShortener(db, gen)
+	if err != nil {
+		panic(err)
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		url, err := short.Resolve(id)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		if url == "" {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		http.Redirect(w, r, url, http.StatusMovedPermanently)
+	})
+
+	mux.HandleFunc("POST /", func(w http.ResponseWriter, r *http.Request) {
+		b := r.Body
+		v := Short{}
+		json.NewDecoder(b).Decode(&v)
+
+		id, err := short.Shorten(v.Url)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		respUrl, err := url.JoinPath("https://", host, id)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		w.Write([]byte(respUrl))
+		w.WriteHeader(http.StatusOK)
+	})
+
+	mux.HandleFunc("GET /health", healthCheck(db))
+	http.ListenAndServe(":8080", mux)
+}
+
+func healthCheck(db *sql.DB) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := db.Ping(); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		w.Write([]byte("OK"))
+		w.WriteHeader(http.StatusOK)
+	}
+}
